a4dca39ffbb2b0b461f395da30d293a48a566413
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-12-09 06:24:12 +0530

    Update FormFooter.tsx

--- src/components/FormFooter.tsx
+++ src/components/FormFooter.tsx
@@ -71,7 +71,7 @@ function FormFooterComp() {
         ...prev,
         _id: res.data as any,
       }));
-      setDraft(false);
+      setDraft(true);
     }
     if (res.error) {
       setDraft(false);
@@ -82,7 +82,7 @@ function FormFooterComp() {
   useEffect(() => {
     setDraft(false);
     saveForm().then(() => {});
-  }, [formBuilderData.questions]);
+  }, [formBuilderData.questions.d, formBuilderData.form_title]);
   return (
     <div className="bg-peerlistBackground z-10 border-t flex p-4 px-6 max-md:px-2 gap-1.5 items-center justify-between w-full">
       <Button
