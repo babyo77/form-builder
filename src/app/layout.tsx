387a49a30bf737dd6a9c016e1ca3cff40e8cd2de
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-12-09 18:28:18 +0530

    added minor seo (openGraph image)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,46 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "PeerList Forms",
   description: "",
+  icons: { icon: "/favicon.ico" },
+
+  // OpenGraph Meta Tags
+  openGraph: {
+    title: "PeerList Forms",
+    description: "Peerlist Frontend Developer Assignment",
+    url: "https://frombuilder.vercel.app",
+    type: "website",
+    siteName: "PeerList Forms", // Add this line for site_name
+    images: [
+      {
+        url: "https://us-east-1.tixte.net/uploads/tanmay111-files.tixte.co/peerlist_logo.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Vibe",
+      },
+    ],
+  },
+
+  // Twitter Meta Tags
+  twitter: {
+    card: "summary_large_image",
+    site: "@tanmay11117",
+    title: "PeerList Forms",
+    description: "Peerlist Frontend Developer Assignment",
+    images: [
+      {
+        url: "https://us-east-1.tixte.net/uploads/tanmay111-files.tixte.co/peerlist_logo.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Vibe",
+      },
+      {
+        url: "https://us-east-1.tixte.net/uploads/tanmay111-files.tixte.co/peerlist_logo.jpeg",
+        width: 800,
+        height: 600,
+        alt: "",
+      },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
